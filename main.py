3976085aab633742424bc4d5c833bab016267612
JASI GRACE I <67458870+jasigrace@users.noreply.github.com>
2021-05-16 06:05:37 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+#Calculator
+
+def add(n1, n2):
+  return n1 + n2
+
+def subtract(n1, n2):
+  return n1 - n2
+
+def multiply(n1, n2):
+  return n1 * n2
+
+def divide(n1, n2):
+  return n1 / n2
+
+operations = {
+  "+" : add,
+  "-" : subtract,
+  "*" : multiply,
+  "/" : divide,
+}
+
+from art import logo
+print(logo)
+
+def calculator():
+  num1 = float(input("What's the first number? "))
+  num2 = float(input("What's the second number? "))
+  for operation in operations:
+    print(operation)
+  operation_symbol = input("Pick an operation from the line above: ")
+
+
+  end_of_operations = True
+  while end_of_operations:
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation.: ") == "y":
+      num1 = answer
+      num2 = float(input("What's the next number? "))
+      operation_symbol = input("Pick an operation: ")
+    else:
+      end_of_operations = False
+      calculator()
+
+calculator()
+      
